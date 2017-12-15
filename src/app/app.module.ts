b9924d640abe5573cde0262250f00d9d00db3ea5
sairatan <saira_tan@hotmail.com>
2017-12-15 15:24:13 +0100

    adding initial homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from 'app/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    RouterModule.forRoot([
+      {
+        path: 'homepage', component: HomepageComponent
+      }
+    ])
   ],
+  exports: [ RouterModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
